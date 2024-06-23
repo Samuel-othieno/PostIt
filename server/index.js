a13cf0a458ae385b16178b4490dc6d1dd795df27
Samuel Douglas Othieno <douglasothieno@gmail.com>
2024-06-23 23:45:19 +0300

    Initial Commit: Add Server side Code

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
-This will house your back-end implementation in NodeJS-Express-Postgres
+// This will house My back-end implementation in NodeJS-Express-Postgres
+import express from 'express';
+import morgan from 'morgan';
+import { StatusCodes } from 'http-status-codes';
+import 'dotenv/config';
+import helmet from "helmet";
+import cors from 'cors';
+
+const App = express();
