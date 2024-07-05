ffeb1affb3076f96f71a342a562a7ab4113e70a1
Samuel Douglas Othieno <douglasothieno@gmail.com>
2024-07-05 23:52:52 +0300

    Add User Middleware

--- server/Middleware/UserValidation.js
+++ server/Middleware/UserValidation.js
@@ -1,26 +1,44 @@
-import { BadRequest, NotFound, ExistingConflict } from "../Classes/Errors.class.js";
+import {
+    BadRequest,
+    NotFound,
+    ExistingConflict,
+} from "../Classes/Errors.class.js";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 async function checkExistingUser(req, res, next) {
-
-    const {userId} = req.body;
+    const { userId } = req.body;
 
     try {
-        const user = await prisma.user.findFirst({
+        const user = await prisma.user.findUnique({
             where: { id: userId },
         });
-    
+
         if (!user) {
-            return next(new NotFound(`${userId} not found`))
-        } 
+            return next(new NotFound(`${userId} not found`));
+        }
 
-        next()
-
+        next();
     } catch (error) {
-        next(error)
+        next(error);
     }
 }
 
-export default checkExistingUser;
+function validateUserLoginInput(req, res, next) {
+    const { username, email, phone } = req.body;
+
+    if ((!username && !email && !phone) || !password) {
+        const errorMessage =
+            !username && !email && !phone
+                ? "Username, phonenumber or Email"
+                : "Password";
+
+        return next(BadRequest(`${errorMessage} is missing!`));
+    }
+    next();
+}
+
+
+
+export { checkExistingUser, validateUserLoginInput };
