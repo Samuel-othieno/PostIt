4457d95581b63d3adf9c88d224125ad6ef613436
Samuel Douglas Othieno <douglasothieno@gmail.com>
2024-07-15 12:38:12 +0300

    ...

--- server/Controllers/groupMessages.controller.js
+++ server/Controllers/groupMessages.controller.js
@@ -10,21 +10,36 @@ import { messages } from "../Messages/messages.js";
 const prisma = new PrismaClient();
 
 async function sendMessageTogroup(req, res) {
-  const { groupId } = req.params;
+  const {groupId} = req.params;
   const { userId, content } = req.body;
 
   try {
+    const parsedGroupId = parseInt(groupId);
+
     const group = await prisma.group.findUnique({
-      where: { id: parseInt(groupId) },
+      where: {id: parsedGroupId},
     });
 
     if (!group) {
       return new NotFound(messages.group.groupDeleted);
     }
-    
+
+    const isMember = await prisma.groupMembers.findUnique({
+        where:{
+            groupId_userId:{
+                groupId: parsedGroupId,
+                userId: userId,
+            },
+        },
+    });
+
+    if (!isMember){
+        return res .status(StatusCodes.FORBIDDEN).json({message:"User is not a member of the Group"})
+    }
+
     const message = await prisma.groupMessages.create({
       data: {
-        groupId: parseInt(groupId),
+        groupId: parsedGroupId,
         userId,
         content,
       },
