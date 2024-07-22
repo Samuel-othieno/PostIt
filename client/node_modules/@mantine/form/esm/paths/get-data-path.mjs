a47733c14fb1f2745867d75a368bfc3397a232ed
Samuel Douglas Othieno <douglasothieno@gmail.com>
2024-07-22 16:53:29 +0300

    ADD VITE

--- client/node_modules/@mantine/form/esm/paths/get-data-path.mjs
+++
@@ -1,7 +0,0 @@
-'use client';
-function getDataPath(formName, fieldPath) {
-  return formName ? `${formName}-${fieldPath.toString()}` : fieldPath.toString();
-}
-
-export { getDataPath };
-//# sourceMappingURL=get-data-path.mjs.map
--- client/node_modules/@mantine/form/lib/hooks/use-form-errors/use-form-errors.d.ts
+++
@@ -1,9 +0,0 @@
-import { ClearErrors, ClearFieldError, FormErrors, SetErrors, SetFieldError } from '../../types';
-export interface $FormErrors<Values extends Record<string, any>> {
-    errorsState: FormErrors;
-    setErrors: SetErrors;
-    clearErrors: ClearErrors;
-    setFieldError: SetFieldError<Values>;
-    clearFieldError: ClearFieldError;
-}
-export declare function useFormErrors<Values extends Record<string, any>>(initialErrors: FormErrors): $FormErrors<Values>;
