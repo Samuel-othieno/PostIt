2315638ff871af43170622235ec54589becab33b
Samuel Douglas Othieno <douglasothieno@gmail.com>
2024-08-05 17:17:20 +0300

    ReduxToolkit

--- Clients/postit/src/store.js
+++ Clients/postit/src/store.js
@@ -1,6 +1,12 @@
-import {createStore} from 'redux';
-import rootReducer from './reducers';
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './reducers/userReducer';
+import groupReducer from './reducers/groupReducer';
 
-const store = createStore(rootReducer)
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+    group: groupReducer,
+  },
+});
 
 export default store;
