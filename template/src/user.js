b1e250b368a41d27116058cffa2e8cb1994a7bc4
SAMUEL DOUGLAS OTHIENO <145870651+Samuel-othieno@users.noreply.github.com>
2024-08-10 16:14:17 +0300

    Delete template directory

--- template/src/user.js
+++
@@ -1,8 +0,0 @@
-export function createUser(username, email, password) {
-    // Function to create a user
-  }
-  
-  export function loginUser(username, password) {
-    // Function to log in a user
-  }
-  
