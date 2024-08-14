68ab6a1a5743dfa90d9a8b60e1c87c2c6a2e5bb4
Samuel Douglas Othieno <douglasothieno@gmail.com>
2024-08-14 21:50:06 +0300

    Remove backend code from client branch

--- Clients/postit/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
-
-function App() {
-    return <MantineProvider>{ }</MantineProvider>;
-}
-
-export default App;
