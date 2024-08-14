696abe99eb4ac7d6629696d35166613b60ebdf65
Samuel Douglas Othieno <douglasothieno@gmail.com>
2024-08-14 21:28:50 +0300

    Remove frontend code from server branch

--- template/src/main.js
+++
@@ -1,5 +0,0 @@
-import { createUser, loginUser } from "./user.js";
-import { createGroup, addUserToGroup } from "./group.js";
-
-createUser("username", "email@example.com", "password");
-createGroup("Group Name");
