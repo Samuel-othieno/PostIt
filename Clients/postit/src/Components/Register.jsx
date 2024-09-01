5f82e0ef5671104971b791d0255059ff8f90da65
Samuel Douglas Othieno <douglasothieno@gmail.com>
2024-09-01 18:07:29 +0300

    ..

--- Clients/postit/src/Components/Register.jsx
+++ Clients/postit/src/Components/Register.jsx
@@ -51,6 +51,7 @@ const Register = () => {
       <button type="submit" className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
         Register
       </button>
+      
     </form>
 
     </div>
