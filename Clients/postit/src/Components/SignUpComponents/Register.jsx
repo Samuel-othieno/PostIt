aa3493f399bc429848f4464f368887a6b367e014
Samuel Douglas Othieno <douglasothieno@gmail.com>
2024-09-05 20:35:57 +0300

    ....

--- Clients/postit/src/Components/SignUpComponents/Register.jsx
+++ Clients/postit/src/Components/SignUpComponents/Register.jsx
@@ -1,20 +1,16 @@
-import { Link, useNavigation } from 'react-router-dom';
+import { Link, useNavigation } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
-import { useSubmit } from 'react-router-dom';
-import { CheckCircle } from "@mui/icons-material";
+import { useSubmit } from "react-router-dom";
+import { CheckCircle, Visibility, VisibilityOff } from "@mui/icons-material";
 import { Cancel } from "@mui/icons-material";
-// import { Visibility } from "@mui/icons-material";
-// import { VisibilityOff } from "@mui/icons-material";
-// import { CircularProgress } from "@mui/material";
-// import { AddBox } from "@mui/icons-material";
-import {Button, Paper, Typography, IconButton } from "@mui/material";
-import { useEffect, useState } from 'react';
-import Square from './Square';
-import { ToastContainer, toast } from 'react-toastify';
-import { FaArrowCircleLeft } from 'react-icons/fa';
-import { HowToReg } from '@mui/icons-material';
-// import 'react-toastify/dist/ReactToastify.css';
-import { validate } from 'email-validator'; // check package for accuuracy
+import {Paper, Typography, IconButton, Button, Box, CircularProgress } from "@mui/material";
+import { useEffect, useState } from "react";
+import Square from "./Square";
+import {toast} from "react-toastify";
+import { FaArrowCircleLeft } from "react-icons/fa";
+import { HowToReg } from "@mui/icons-material";
+import { validate } from "email-validator"; // check package for accuuracy
+import Input from "./Input";
 
 const PasswordRequirements = ({ password }) => {
   const specialCharacterRegex = /[!@#$%^&*(),.?":{}|<>]/;
@@ -22,37 +18,62 @@ const PasswordRequirements = ({ password }) => {
   const uppercaseRegex = /[A-Z]/;
   const lowercaseRegex = /[a-z]/;
 
-
   const boxStyle = {
-    border: '1px solid #ccc',
-    padding: '8px',
-    borderRadius: '4px',
-    marginTop: '8px',
-    backgroundColor: '#f9f9f9'
+    border: "1px solid #ccc",
+    padding: "8px",
+    borderRadius: "4px",
+    marginTop: "8px",
+    backgroundColor: "#f9f9f9",
   };
 
   return (
     <>
       <div style={boxStyle}>
         <ul>
-          <li style={{color: password.length >= 8 && password.length <= 12 ? 'green' : 'red'}}>
-            {password.length > 8 && password.length <=12 ? <CheckCircle/> : <Cancel/>} 8-12 Characters
-          </li>
-          
-          <li style={{color: numberRegex.test(password)? 'green' : 'red'}}>
-            {numberRegex.test(password)? <CheckCircle/> : <Cancel/>} At least one number
-          </li>
-
-          <li style={{color: uppercaseRegex.test(password)? 'green' : 'red'}}>
-            {uppercaseRegex.test(password)? <CheckCircle/> : <Cancel/>} At least one uppercase letter
-          </li>
-
-          <li style={{color: lowercaseRegex.test(password)? 'green' : 'red'}}>
-            {lowercaseRegex.test(password)? <CheckCircle/> : <Cancel/>} Atleast one lowercase letter
+          <li
+            style={{
+              color:
+                password.length >= 8 && password.length <= 12 ? "green" : "red",
+            }}
+          >
+            {password.length > 8 && password.length <= 12 ? (
+              <CheckCircle />
+            ) : (
+              <Cancel />
+            )}{" "}
+            8-12 Characters
           </li>
 
-          <li style={{color: specialCharacterRegex.test(password)? 'green' : 'red'}}>
-            {specialCharacterRegex.test(password)? <CheckCircle/>:<Cancel/>} At least one special Character
+          <li style={{ color: numberRegex.test(password) ? "green" : "red" }}>
+            {numberRegex.test(password) ? <CheckCircle /> : <Cancel />} At least
+            one number
+          </li>
+
+          <li
+            style={{ color: uppercaseRegex.test(password) ? "green" : "red" }}
+          >
+            {uppercaseRegex.test(password) ? <CheckCircle /> : <Cancel />} At
+            least one uppercase letter
+          </li>
+
+          <li
+            style={{ color: lowercaseRegex.test(password) ? "green" : "red" }}
+          >
+            {lowercaseRegex.test(password) ? <CheckCircle /> : <Cancel />}{" "}
+            Atleast one lowercase letter
+          </li>
+
+          <li
+            style={{
+              color: specialCharacterRegex.test(password) ? "green" : "red",
+            }}
+          >
+            {specialCharacterRegex.test(password) ? (
+              <CheckCircle />
+            ) : (
+              <Cancel />
+            )}{" "}
+            At least one special Character
           </li>
         </ul>
       </div>
@@ -60,64 +81,67 @@ const PasswordRequirements = ({ password }) => {
   );
 };
 
-
 export default function Register() {
   const navigation = useNavigation();
   const submit = useSubmit();
-  const [signUpData, setSignUpData] = useState({name: '', email: '', password: ''});
+  const [signUpData, setSignUpData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
   const [submitting, setSubmitting] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
-  const [showPasswordRequirements, setShowPasswordRequirements] = useState(false);
+  const [showPasswordRequirements, setShowPasswordRequirements] =
+    useState(false);
 
-
-  useEffect(()=> {
- // Update the component when password changes
+  useEffect(() => {
+    // Update the component when password changes
   }, [signUpData.password]);
 
-  function sendData(e, googleauth){ // Look more into Google auth
+  function sendData(e, googleauth) {
+    // Look more into Google auth
     setSubmitting(true);
-    setTimeout(()=>{
+    setTimeout(() => {
       setSubmitting(false);
     }, 5000);
 
-    if (googleauth){
-      submit(googleauth, {method: 'post'});
+    if (googleauth) {
+      submit(googleauth, { method: "post" });
       return;
     }
 
     e.preventdefault();
-    setShowPasswordRequirements(true); 
-
+    setShowPasswordRequirements(true);
 
     if (!signUpData.email || !signUpData.password) {
-      return toast.error('Please fill all required fields', {
-        position: 'top-center',
+      return toast.error("Please fill all required fields", {
+        position: "top-center",
         autoClose: 2500,
         hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
-        theme: 'colored',
+        theme: "colored",
         draggablePercent: true,
       });
     }
 
-    if (!validate(signUpData.email)){
+    if (!validate(signUpData.email)) {
       return toast.error("please enter a Valid Email", {
-        theme: 'colored',
-        position: 'bottom-center',
+        theme: "colored",
+        position: "bottom-center",
         progress: true,
         autoClose: 2500,
         closeOnClick: true,
         closeButton: true,
-        hideProgressBar:false,
+        hideProgressBar: false,
         draggable: true,
       });
     }
 
-    if(signUpData.password.length > 0 && signUpData.password.length < 8) {
-      return toast.error('Invalid! Password must be more than 8 Characters',  {
+    if (signUpData.password.length > 0 && signUpData.password.length < 8) {
+      return toast.error("Invalid! Password must be more than 8 Characters", {
         position: "top-center",
         autoClose: 2500,
         hideProgressBar: false,
@@ -125,12 +149,12 @@ export default function Register() {
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
-        theme: "colored"
+        theme: "colored",
       });
     }
 
     if (signUpData.password.length > 12) {
-      return toast.error('invalid! password should not exceed 12',  {
+      return toast.error("invalid! password should not exceed 12", {
         position: "top-center",
         autoClose: 2500,
         hideProgressBar: false,
@@ -138,7 +162,7 @@ export default function Register() {
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
-        theme: "colored"
+        theme: "colored",
       });
     }
 
@@ -146,8 +170,8 @@ export default function Register() {
     const numberRegex = /[0-9]/;
     const uppercaseRegex = /[A-Z]/;
 
-    if(!numberRegex.test(signUpData.password)){
-      return toast.error('Invaid! Password should have at least one digit', {
+    if (!numberRegex.test(signUpData.password)) {
+      return toast.error("Invaid! Password should have at least one digit", {
         position: "top-center",
         autoClose: 2500,
         hideProgressBar: false,
@@ -155,12 +179,12 @@ export default function Register() {
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
-        theme: "colored"
-      })
+        theme: "colored",
+      });
     }
 
-    if(!specialCharacterRegex.test(signUpData.password)){
-      return toast.error('Invaid! Password should have at least one digit', {
+    if (!specialCharacterRegex.test(signUpData.password)) {
+      return toast.error("Invaid! Password should have at least one digit", {
         position: "top-center",
         autoClose: 2500,
         hideProgressBar: false,
@@ -168,12 +192,12 @@ export default function Register() {
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
-        theme: "colored"
-      })
+        theme: "colored",
+      });
     }
 
-    if(!uppercaseRegex.test(signUpData.password)){
-      return toast.error('Invaid! Password should have at least one digit', {
+    if (!uppercaseRegex.test(signUpData.password)) {
+      return toast.error("Invaid! Password should have at least one digit", {
         position: "top-center",
         autoClose: 2500,
         hideProgressBar: false,
@@ -181,46 +205,104 @@ export default function Register() {
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
-        theme: "colored"
-      })
+        theme: "colored",
+      });
     }
   }
 
-  const responseMessage = (response) =>{
-    let token = response.credential;   //Should use the credentials from Server
+  const responseMessage = (response) => {
+    let token = response.credential; //Should use the credentials from Server
     let decoded = jwtDecode(token);
-    setSignUpData({name: decoded.name, email: decoded.email, password: decoded.sub}); // look more into Sub && JWT Decode
-    sendData(1, {name: decoded.name, email: decoded.email, password: decoded.sub, isGoogle: true, pic: decoded.picture});
+    setSignUpData({
+      name: decoded.name,
+      email: decoded.email,
+      password: decoded.sub,
+    }); // look more into Sub && JWT Decode
+    sendData(1, {
+      name: decoded.name,
+      email: decoded.email,
+      password: decoded.sub,
+      isGoogle: true,
+      pic: decoded.picture,
+    });
   };
 
   const errorMessage = (error) => {
     console.log(error);
-  }
+  };
 
-  const handleCLickShowPassword = ()=> setShowPassword
-  (!showPassword);
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
   const handleMouseDownPassword = (event) => event.preventdefault();
-  
+
   return (
     <>
-    <div className='flex flex-col items-center h-screen w-screen relative overflow-hidden px-2'>
-      <Square></Square>
-      <Square isRight={true}></Square>
-      <Paper className='z-20 w-full max-w-[370px] p-[2rem] my-auto' elevation={3}>
-        <Link to='/'><FaArrowCircleLeft className='text-blue-600 cursor-pointer text-2xl'></FaArrowCircleLeft></Link>
-        <div className='font-Poppins text-3xl font-extrabold flex items-center flex-col'>
-          <HowToReg fontSize='large' color='primary'/>
-          <Typography variant='h5'>Sign Up</Typography>/
-        </div>
-        <br />
+      <div className="flex flex-col items-center h-screen w-screen relative overflow-hidden px-2">
+        <Square></Square>
+        <Square isRight={true}></Square>
+        <Paper
+          className="z-20 w-full max-w-[370px] p-[2rem] my-auto"
+          elevation={3}
+        >
+          <Link to="/">
+            <FaArrowCircleLeft className="text-blue-600 cursor-pointer text-2xl"></FaArrowCircleLeft>
+          </Link>
+          <div className="font-Poppins text-3xl font-extrabold flex items-center flex-col">
+            <HowToReg fontSize="large" color="primary" />
+            <Typography variant="h5">Sign Up</Typography>/
+          </div>
+          <br />
+          <hr /> <hr />
+          <form className="mt-6 relative"></form>
+          <Input
+            onSetData={setSignUpData}
+            name="name"
+            text="Name"
+            placeholder="Enter your name"
+            type="text"
+          ></Input>
+          <Input
+            onSetData={setSignUpData}
+            name="email"
+            text="Email ID"
+            placeholder="Enter your Email Address"
+            type="text"
+          ></Input>
+          <div className="relativ">
+            <div className="relative">
+              <Input
+                onSetData={setSignUpData}
+                name="password"
+                text="Password"
+                type={showPassword ? "text" : "password"}
+                placeholder="password"
+              />
 
-        <hr /> <hr />
+              <IconButton  aria-label='toggle password visibility'
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge='end'
+                style={{ position: 'absolute', right: '10px', top: '71%', transform: 'translateY(-50%)' }}>
+                {showPassword? <VisibilityOff/> : <Visibility/>}
+              </IconButton>
+            </div>
+            {showPasswordRequirements && <PasswordRequirements password={signUpData.password} />}
+          </div>
 
+          <div className="flex flex-row justify-center mt-8">
+            <Button sx={{padding: ".5rem 4rem"}} onClick={sendData} variant="contained">
+              {!submitting && <div>SIGN UP</div>}
+              {submitting && <Box sx={{display: 'flex'}}><CircularProgress size={25} style={{color:'#FFFFFF'}}/></Box>}
+            </Button>
+          </div>
 
-        <form className='mt-6 relative'></form>
-      </Paper>
+          <Typography className="text-center py-3">Already have an account? <Link className='text-blue-600' to='/login'>login</Link></Typography>
 
-    </div>
+          <div className="h-[1px] w-full mt-4 bg-gray-400"></div>
+          <div className="flex flex-col items-center mt-6">
+            <GoogleLogin onSuccess/>
+          </div>
+        </Paper>
+      </div>
     </>
-  )
+  );
 }
