58f952a788f3633ac83db752c0e9942e4d28a984
Samuel Douglas Othieno <douglasothieno@gmail.com>
2024-09-28 14:54:41 +0300

    Add NavList & Bar

---
+++ Clients/postit/src/Components/HomeComponents/NavList.jsx
@@ -0,0 +1,58 @@
+import { NavLink } from "react-router-dom";
+
+export default function NavList() {
+  function ScrollDown() {
+    const value = window.innerHeight;
+    window.scrollTo({ top: value, behavior: "smooth" });
+  }
+
+  function onToggleMenu(e) {
+    e.target.name = e.target.name === "grid" ? "close" : "grid";
+
+    const menulinks = document.querySelector(".menulinks");
+    menulinks.classList.toggle("top-[8%]");
+  }
+
+  return (
+    <div>
+      <div
+        id="menulink"
+        className="menulinks duration-500 md:static md:min-h-fit absolute min-h-[28vh] left-[0%] top-[-100%] md:w-auto w-[100%] flex justify-center py-5"
+      >
+        <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8 text-white font-Roboto font-semibold ">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "text-[#FFCC33]" : "")}
+            >
+              Product
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              onClick={() => {
+                ScrollDown();
+              }}
+            >
+              Services
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+          <li>
+            <NavLink to="/signup">Signup</NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="flex items-center gap-6">
+        <ion-icon
+          name="grid"
+          class="cursor-pointer md:hidden"
+          onClick={onToggleMenu}
+        ></ion-icon>
+      </div>
+    </div>
+  );
+}
