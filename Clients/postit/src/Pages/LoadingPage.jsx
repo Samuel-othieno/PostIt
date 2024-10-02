83005207443bd8189523af41b4df7416d825d677
Samuel Douglas Othieno <douglasothieno@gmail.com>
2024-10-02 22:40:24 +0300

    Add Home, Loading & Services Pages

---
+++ Clients/postit/src/Pages/LoadingPage.jsx
@@ -0,0 +1,11 @@
+import LoadingGif from "../assets/images/loading.gif"; // gif Image 
+
+export default function LoadingPage() {
+  return (
+    <div className="flex justify-center items-center h-[100vh]">
+      <div className="w-[80vh] flex justify-center">
+        <img src={LoadingGif} alt="Loading..." />
+      </div>
+    </div>
+  );
+}
