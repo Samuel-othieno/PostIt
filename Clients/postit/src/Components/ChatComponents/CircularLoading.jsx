8b9478169c0625e65cea04f00bd5cf3b76507b25
Samuel Douglas Othieno <douglasothieno@gmail.com>
2024-10-10 20:17:17 +0300

    Add EmptyMessage, Circular Loading  & Advertisement

---
+++ Clients/postit/src/Components/ChatComponents/CircularLoading.jsx
@@ -0,0 +1,9 @@
+import { CircularProgress } from '@mui/material'
+
+export default function CircularLoading() {
+  return (
+    <div className='h-[100%] w-[100%] flex justify-center items-center'>
+        <CircularProgress />
+    </div>
+  )
+}
