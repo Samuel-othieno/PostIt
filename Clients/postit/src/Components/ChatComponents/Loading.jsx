2671e6cfaaf393b18f7192649463a14177208873
Samuel Douglas Othieno <douglasothieno@gmail.com>
2024-10-14 20:54:11 +0300

    Add ReceiverMessage

---
+++ Clients/postit/src/Components/ChatComponents/Loading.jsx
@@ -0,0 +1,9 @@
+import { Skeleton } from '@mui/material'
+export default function Loading() {
+  return (
+    <div className='w-[100%] border-[#acacac] flex flex-col'>
+        <Skeleton height={70}  />
+        <Skeleton height={70} />
+    </div>
+  )
+}
